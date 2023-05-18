567a04718593edd02b33c4497029c098fb6ca63d
VikasSahu1998 <sahuvikas1998@gmail.com>
2023-05-18 13:05:58 +0530

    first commit

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -8,18 +8,18 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
   postEmp(data: any) {
-    return this.http.post<any>("http://localhost:3000/emplist/", data);
+    return this.http.post<any>("http://localhost:3000/users/", data);
   }
 
   getEmp() {
-    return this.http.get<any>("http://localhost:3000/emplist/");
+    return this.http.get<any>("http://localhost:3000/users/");
   }
 
   putEmp(data: any, id: number) {
-    return this.http.put<any>("http://localhost:3000/emplist/" + id, data);
+    return this.http.put<any>("http://localhost:3000/users/" + id, data);
   }
 
   deleteEmp(id: number) {
-    return this.http.delete<any>("http://localhost:3000/emplist/" + id);
+    return this.http.delete<any>("http://localhost:3000/users/" + id);
   }
 }
